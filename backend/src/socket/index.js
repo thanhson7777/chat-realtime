b8b7844fbb090bc149e140a2695dad78feada5f0
thanhson7777 <thanhson11052003@gmail.com>
2026-03-08 15:47:29 +0700

    init project

---
+++ backend/src/socket/index.js
@@ -0,0 +1,46 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import { socketAuthMiddleware } from "../middlewares/socketMiddleware.js";
+import { getUserConversationsForSocketIO } from "../controllers/conversationController.js";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
+});
+
+io.use(socketAuthMiddleware);
+
+const onlineUsers = new Map();
+
+io.on("connection", async (socket) => {
+  const user = socket.user;
+
+  onlineUsers.set(user._id, socket.id);
+
+  io.emit("online-users", Array.from(onlineUsers.keys()));
+
+  const conversationIds = await getUserConversationsForSocketIO(user._id);
+  conversationIds.forEach((id) => {
+    socket.join(id);
+  });
+
+  socket.on("join-conversation", (conversationId) => {
+    socket.join(conversationId);
+  });
+
+  socket.join(user._id.toString());
+
+  socket.on("disconnect", () => {
+    onlineUsers.delete(user._id);
+    io.emit("online-users", Array.from(onlineUsers.keys()));
+  });
+});
+
+export { io, app, server };
